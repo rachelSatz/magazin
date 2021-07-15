f1e90f541e15084a2c87b46d6c0b150251b0c3ed
rachelsin <rmpodo@gmail.com>
2021-07-15 13:18:35 +0300

    update

--- model/magazin.js
+++ model/magazin.js
@@ -8,13 +8,7 @@ const MagazinSchema = mongoose.Schema({
     date: {
         type: Date, default: Date.now
     },
-<<<<<<< HEAD
     images:{type:String},
-=======
-    images:{
-        type: Image
-    },
->>>>>>> rachellsatz
     posts: [
         { type: mongoose.Types.ObjectId, ref: 'Post' }
     ]
